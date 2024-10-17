b87942ac90919ba3f1f4ad68ace657ce33600925
igarashi kazuki <sousiki.links@gmail.com>
2024-10-17 23:22:35 +0900

    update: ドロワー用のJSとアニメーションを追加

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-document.querySelectorAll('.nav__link').forEach(item => {
-  item.addEventListener('click', function(e) {
-    e.preventDefault();
+$(function () {
+  document.querySelectorAll(".nav__link").forEach((item) => {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
 
-    document.querySelectorAll('.nav__link').forEach(link => {
-      link.classList.remove('active');
+      document.querySelectorAll(".nav__link").forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      this.classList.add("active");
     });
+  });
 
-    this.classList.add('active');
+  $("#js-drawer-icon").on("click", function () {
+    $(this).toggleClass("is-checked");
+    $("#js-drawer-content").toggleClass("is-checked");
+  });
+
+  $('#js-drawer-content a[href^="#"]').on("click", function () {
+    $("#js-drawer-icon").removeClass("is-checked");
+    $("#js-drawer-content").removeClass("is-checked");
   });
 });
